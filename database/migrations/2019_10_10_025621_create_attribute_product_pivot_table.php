7d548489dd0acb6ad3c477d50331d9dbb4588472
Martín Vásquez <pedromvasquezb@gmail.com>
2019-10-09 21:58:58 -0500

    Add product attribute model and migration

---
+++ database/migrations/2019_10_10_025621_create_attribute_product_pivot_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeProductPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_product', function (Blueprint $table) {
+            $table->bigInteger('attribute_id')->unsigned()->index();
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
+            $table->bigInteger('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->primary(['attribute_id', 'product_id'], 'prod_attr_primary');
+            $table->string('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_product');
+    }
+}
